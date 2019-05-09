f80a031110add3d77e4da4f2f5abe1dc6cd33b5d
Abdulquadri <olayanju.olaide@olivetsolutions>
2019-05-09 13:30:31 +0100

    First commit

---
+++ precache-manifest.48a127164588967114247a7d8c7ea3f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "812989caa3438ef792b20cc2922c4e54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "5571b7bfa8d288189638",
+    "url": "/static/css/main.0cf1ee40.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "5571b7bfa8d288189638",
+    "url": "/static/js/main.4814fbcc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
